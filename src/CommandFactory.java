0a4f8a761ac0ce5abd67f0b3f95c63017e82790d
trikitrok <trikitrok@gmail.com>
2016-04-05 01:00:40 +0200

    Refining names

--- src/CommandFactory.java
+++ src/CommandFactory.java
@@ -1,32 +1,32 @@
 public class CommandFactory {
-    public static Command createFrom(String commandRepresentation) {
-        if (isLeftRotation(commandRepresentation)) {
+    public static Command createFrom(String representation) {
+        if (isLeftRotation(representation)) {
             return new LeftRotation();
-        } else if (isRightRotation(commandRepresentation)) {
+        } else if (isRightRotation(representation)) {
             return new RightRotation();
         } else {
-            int displacement = extractDisplacement(commandRepresentation);
+            int displacement = extractDisplacement(representation);
             return new Displacement(displacement);
         }
     }
 
-    private static int extractDisplacement(String commandRepresentation) {
+    private static int extractDisplacement(String representation) {
         final int DISPLACEMENT_LENGTH = 1;
-        if (isForwardsDisplacement(commandRepresentation)) {
+        if (isForwardsDisplacement(representation)) {
             return DISPLACEMENT_LENGTH;
         }
         return -DISPLACEMENT_LENGTH;
     }
 
-    private static boolean isForwardsDisplacement(String commandRepresentation) {
-        return commandRepresentation.equals("f");
+    private static boolean isForwardsDisplacement(String representation) {
+        return representation.equals("f");
     }
 
-    private static boolean isRightRotation(String command) {
-        return command.equals("r");
+    private static boolean isRightRotation(String representation) {
+        return representation.equals("r");
     }
 
-    private static boolean isLeftRotation(String command) {
-        return command.equals("l");
+    private static boolean isLeftRotation(String representation) {
+        return representation.equals("l");
     }
 }
